6eb1fc8a552441c83c0e05bc7a066c0081445a19
mike dank <famicoman@gmail.com>
2017-08-14 11:29:13 +0000

    Updated for latest rtmbot version

--- README.md
+++ README.md
@@ -5,20 +5,54 @@ Slack bot plugin to offer downloading/archiving utilities through slack
 
 **Written in python**
 
-Using slackhq/python-rtmbot
+Using slackapi/python-rtmbot
 Using python 2.7.3
 
-Visit https://github.com/slackhq/python-rtmbot for python-rtmbot info
+Visit https://github.com/slackapi/python-rtmbot for python-rtmbot info
 
 ## Installation
 
-Add this folder to your python-rtmbot/plugins folder
+Install pip, then install rtmbot
+
+	sudo easy_install pip
+	sudo pip install rtmbot
+
+Make a project folder for your rtmbot instance
+
+	mkdir ~/rtmbot
+	cd ~/rtmbot
+
+Make a plugins directory and touch an init module
+
+	mkdir plugins
+	touch plugins/__init__.py
+
+Clone this repo and copy rtmbot-archivebotjr.py to your plugins directory
+
+	git clone https://github.com/Famicoman/rtmbot-archivebotjr.git
+	cp rtmbot-archivebotjr/rtmbot-archivebotjr.py plugins/rtmbot-archivebotjr.py
+
+Edit your rtmbot.conf to become aware of the new plugin
+
+	nano rtmbot.conf
+
+It should look similar to the below
+
+	DEBUG: True # make this False in production
+ 	SLACK_TOKEN: "xoxb-your-token"
+ 	ACTIVE_PLUGINS:## Usage
+	    - plugins.rtmbot-archivebotjr.ArchivebotJrPlugin
 
 ## Usage
 
-Start python-rtmbot normally using sudo
+Start rtmbot normally within your project folder
 
-	sudo ./rtmbot.py
+	cd ~/rtmbot
+	rtmbot
+
+Check logs if necessary
+
+	tail -f ~/rtmbot/rtmbot.log
 
 ## Commands (entered into slack channel)
 
@@ -36,9 +70,9 @@ Run wget with continue option.
 
 	!torsocks-wget <url>
 
-Run wget through torsocks, you can download files on onion sites.
+Run wget through torsocks, you can download files on onion sites. You must have torsocks installed!
 
 	!youtube-dl <url>
 
-Run youtube-dl on target video, with highest quality.
+Run youtube-dl on target video, with highest quality. You must have youtube-dl installed!
 
--- rtmbot-archivebotjr.py
+++ rtmbot-archivebotjr.py
@@ -1,117 +1,120 @@
 #### rtmbot-archivebotjr.py
 #### By Mike Dank (@Famicoman)
-#### Version 1.0 (2016-02-15)
+#### Version 1.1 (2017-08-14)
 #### Slack bot used for archiving material
 
 import os
 import pickle
 import subprocess
 import commands
+from rtmbot.core import Plugin
 
-outputs = []
+class ArchivebotJrPlugin(Plugin):
 
-### process_message
-### Boilerplate slack code for channel message processing
-def process_message(data):
-    channel = data["channel"]
-    text = data["text"]##.encode('ascii','ignore')
-    # Accept tasks on all channels
-    if channel.startswith("D") or channel.startswith("C"):
-        try:
-            if text.startswith("!youtubedl"):
-                cmdYoutubedl(text)
-            elif text.startswith("!uptime"):
-                cmdUptime(text)
-            elif text.startswith("!wget"):
-                cmdWget(text)
-            elif text.startswith("!torsocks-wget"):
-                cmdTorsockswget(text)
-            elif text.startswith("!ping"):
-                cmdPing(text)
-        except:
-            print "Error! Attempting recovery..."
-            raise
-
-### sanitize
-### Sanitize the user input, we don't want them to execute any crazy commands
-def sanitize(inputStr):
-	return inputStr.strip(";|><`'&\\\"")
-
-### logToChannel
-### Log output to the slack channel
-def logToChannel(inputStr):
-    outputs.append([channel, inputStr])
-
-### logToConsole
-### Log output to the local console
-def logToConsole(inputStr):
-    print inputStr
-
-### cmdYoutubedl
-### Run youtube-dl on target video, with highest quality
-### This requires a fairly recent build of ffmpeg
-### YOU MUST SET FFMPEG_HOME VARIABLE
-def cmdYoutubedl(inputStr):
-    logToConsole("[youtube-dl] inputStr: " + inputStr)
-    FFMPEG_HOME = "/home/slackbot/bin"
-    array = inputStr.split(" ")
-    arg = array[1].encode('ascii','ignore').replace('<',"").replace('>',"")
-    if '|' in arg:
-        arg = arg.split('|')[0]
-    arg = arg.replace("https:", "http:")
-    logToConsole("[youtube-dl] Using link: " + arg)
-    version = commands.getstatusoutput('youtube-dl --version')
-    logToConsole("[youtube-dl] version: " + version[1])
-    logToChannel("Version: " + version[1])
-    arg = sanitize(arg)
-    log = commands.getstatusoutput("youtube-dl --prefer-ffmpeg --ffmpeg-location " + FFMPEG_HOME + " --title --continue --retries 4 --write-info-json --write-description --write-thumbnail --write-annotations --all-subs --ignore-errors -f bestvideo+bestaudio " + arg)
-    logToConsole("[youtube-dl] " + log)
-    logToChannel(log[1])
-
-### cmdUptime
-### Run the uptime command
-def cmdUptime(inputStr):
-    logToConsole("[uptime] inputStr: " + inputStr)
-    log = commands.getstatusoutput("uptime")
-    logToConsole("[uptime] " + log)
-    logToChannel(log[1])
-
-### cmdWget
-### Run wget with the continue option	
-def cmdWget(inputStr):
-    logToConsole("[wget] inputStr: " + inputStr)
-    array = inputStr.split(" ")
-    arg = array[1].replace('<',"").replace('>',"")
-    if '|' in arg:
-        arg = arg.split('|')[0]
-    arg = sanitize(arg)
-    log = commands.getstatusoutput("wget -c " + arg)
-    logToConsole("[wget] " + log)
-    logToChannel(log[1])
-
-### cmdTorsockswget
-### Run wget through torsocks
-### YOU MUST HAVE torsocks INSTALLED
-def cmdTorsockswget(inputStr):
-    logToConsole("[torsocks-wget] inputStr: " + inputStr)
-    array = inputStr.split(" ")
-    arg = array[1].replace('<',"").replace('>',"")
-    if '|' in arg:
-        arg = arg.split('|')[0]
-    arg = sanitize(arg)
-    log = commands.getstatusoutput("torsocks wget -c " + arg)
-    logToConsole("[torsocks-wget] " + log)
-    logToChannel(log[1])
-
-### cmdPing
-### Runs IPv4 ping four times on target
-def cmdPing(inputStr):
-    logToConsole("[ping] inputStr: " + inputStr)
-    array = inputStr.split(" ")
-    arg = array[1].replace('<',"").replace('>',"")
-    if '|' in arg:
-        arg = arg.split('|')[1]
-    arg = sanitize(arg)
-    log = commands.getstatusoutput("ping -c 4 " + arg)
-    logToConsole("[ping] " + log)
-    logToChannel(log[1])
+	channel = ""
+	
+	### process_message
+	### Boilerplate slack code for channel message processing
+	def process_message(self, data):
+	    self.channel = data["channel"]
+	    text = data["text"]##.encode('ascii','ignore')
+	    # Accept tasks on all channels
+	    if self.channel.startswith("D") or self.channel.startswith("C"):
+	        try:
+	            if text.startswith("!youtubedl"):
+	                self.cmdYoutubedl(text)
+	            elif text.startswith("!uptime"):
+	                self.cmdUptime(text)
+	            elif text.startswith("!wget"):
+	                self.cmdWget(text)
+	            elif text.startswith("!torsocks-wget"):
+	                self.cmdTorsockswget(text)
+	            elif text.startswith("!ping"):
+	                self.cmdPing(text)
+	        except:
+	            print "Error! Attempting recovery..."
+	            raise
+	
+	### sanitize
+	### Sanitize the user input, we don't want them to execute any crazy commands
+	def sanitize(self, inputStr):
+		return inputStr.strip(";|><`'&\\\"")
+	
+	### logToChannel
+	### Log output to the slack channel
+	def logToChannel(self, inputStr):
+	    self.outputs.append([self.channel, inputStr])
+	
+	### logToConsole
+	### Log output to the local console
+	def logToConsole(self, inputStr):
+	    print inputStr
+	
+	### cmdYoutubedl
+	### Run youtube-dl on target video, with highest quality
+	### This requires a fairly recent build of ffmpeg
+	### YOU MUST SET FFMPEG_HOME VARIABLE
+	def cmdYoutubedl(self, inputStr):
+	    self.logToConsole("[youtube-dl] inputStr: " + inputStr)
+	    FFMPEG_HOME = "/home/slackbot/bin"
+	    array = inputStr.split(" ")
+	    arg = array[1].encode('ascii','ignore').replace('<',"").replace('>',"")
+	    if '|' in arg:
+	        arg = arg.split('|')[0]
+	    arg = arg.replace("https:", "http:")
+	    self.logToConsole("[youtube-dl] Using link: " + arg)
+	    version = commands.getstatusoutput('youtube-dl --version')
+	    self.logToConsole("[youtube-dl] version: " + version[1])
+	    self.logToChannel("Version: " + version[1])
+	    arg = self.sanitize(arg)
+	    log = commands.getstatusoutput("youtube-dl --prefer-ffmpeg --ffmpeg-location " + FFMPEG_HOME + " --title --continue --retries 4 --write-info-json --write-description --write-thumbnail --write-annotations --all-subs --ignore-errors -f bestvideo+bestaudio " + arg)
+	    self.logToConsole("[youtube-dl] " + str(log))
+	    self.logToChannel(log[1])
+	
+	### cmdUptime
+	### Run the uptime command
+	def cmdUptime(self, inputStr):
+	    self.logToConsole("[uptime] inputStr: " + inputStr)
+	    log = commands.getstatusoutput("uptime")
+	    self.logToConsole("[uptime] " + str(log))
+	    self.logToChannel(log[1])
+	
+	### cmdWget
+	### Run wget with the continue option	
+	def cmdWget(self, inputStr):
+	    self.logToConsole("[wget] inputStr: " + inputStr)
+	    array = inputStr.split(" ")
+	    arg = array[1].replace('<',"").replace('>',"")
+	    if '|' in arg:
+	        arg = arg.split('|')[0]
+	    arg = self.sanitize(arg)
+	    log = commands.getstatusoutput("wget -c " + arg)
+	    self.logToConsole("[wget] " + str(log))
+	    self.logToChannel(log[1])
+	
+	### cmdTorsockswget
+	### Run wget through torsocks
+	### YOU MUST HAVE torsocks INSTALLED
+	def cmdTorsockswget(self, inputStr):
+	    self.logToConsole("[torsocks-wget] inputStr: " + inputStr)
+	    array = inputStr.split(" ")
+	    arg = array[1].replace('<',"").replace('>',"")
+	    if '|' in arg:
+	        arg = arg.split('|')[0]
+	    arg = self.sanitize(arg)
+	    log = commands.getstatusoutput("torsocks wget -c " + arg)
+	    self.logToConsole("[torsocks-wget] " + str(log))
+	    self.logToChannel(log[1])
+	
+	### cmdPing
+	### Runs IPv4 ping four times on target
+	def cmdPing(self, inputStr):
+	    self.logToConsole("[ping] inputStr: " + inputStr)
+	    array = inputStr.split(" ")
+	    arg = array[1].replace('<',"").replace('>',"")
+	    if '|' in arg:
+	        arg = arg.split('|')[1]
+	    arg = self.sanitize(arg)
+	    log = commands.getstatusoutput("ping -c 4 " + arg)
+	    self.logToConsole("[ping] " + str(log))
+	    self.logToChannel(log[1])
